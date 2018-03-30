28972983f92fd394afa67ae1c4d30c48c649b57b
Katy DeCorah <katydecorah@gmail.com>
2018-03-30 13:26:25 -0400

    :wrench: clean up

--- index.js
+++ index.js
@@ -5,28 +5,22 @@ const moment = require('moment');
 const _ = require('underscore');
 
 const chill = (event, context, callback) => {
-  if (!process.env.GithubAccessToken) throw new Error('Github token must be set via GithubAccessToken env vars');
-  if (!process.env.SlackHookURL) throw new Error('Slack hook URL must be set via SlackHookURL env vars');
+  if (!process.env.GithubAccessToken)
+    throw new Error('Github token must be set via GithubAccessToken env vars');
+  if (!process.env.SlackHookURL)
+    throw new Error('Slack hook URL must be set via SlackHookURL env vars');
 
-  module.exports.getJSON()
-  .catch(err => callback(err))
-  .then(module.exports.getEvents)
-  .then(module.exports.getDates)
-  .then(module.exports.getDaysInRow)
-  .then(module.exports.post)
-  .then(chill => callback(null, chill))
-  .catch(err => callback(err));
+  module.exports
+    .getJSON()
+    .then(module.exports.getEvents)
+    .then(module.exports.getDates)
+    .then(module.exports.getDaysInRow)
+    .then(module.exports.post)
+    .then(chill => callback(null, chill))
+    .catch(err => callback(err));
 };
 
-/**
-* Posts chill message to Slack webhook URL.
-*
-* @function post
-* @param {String} message - Message that reminds me to chill
-* @param {Callback} callback - if error, returns error message; otherwise,
-* returns request body
-*/
-const post = (message) => {
+const post = message => {
   const json = {
     channel: process.env.SlackChannel,
     username: 'ChillBot',
@@ -38,14 +32,18 @@ const post = (message) => {
 
   return new Promise((resolve, reject) => {
     if (message) {
-      request.post({
-        url: process.env.SlackHookURL,
-        json: json
-      }, (err, resp) => {
-        if (err) reject(err);
-        if (resp.statusCode !== 200) reject(`Got HTTP status ${resp.statusCode} from Slack`);
-        return resolve('Posted to Slack.');
-      });
+      request.post(
+        {
+          url: process.env.SlackHookURL,
+          json: json
+        },
+        (err, resp) => {
+          if (err) reject(err);
+          if (resp.statusCode !== 200)
+            reject(`Got HTTP status ${resp.statusCode} from Slack`);
+          return resolve('Posted to Slack.');
+        }
+      );
     } else {
       resolve('No streak, no Slack post.');
     }
@@ -55,12 +53,14 @@ const post = (message) => {
 const getJSON = () => {
   let results = [];
   return new Promise((resolve, reject) => {
-    const recurse = (page) => {
+    const recurse = page => {
       let opts = {
-        url: `https://api.github.com/users/${process.env.GithubUsername}/events?page=${page}`,
+        url: `https://api.github.com/users/${
+          process.env.GithubUsername
+        }/events?page=${page}`,
         headers: {
           'User-Agent': process.env.GithubUsername,
-          'Authorization': `token ${process.env.GithubAccessToken}`
+          Authorization: `token ${process.env.GithubAccessToken}`
         }
       };
       return request(opts, (err, res, body) => {
@@ -79,41 +79,50 @@ const getJSON = () => {
   });
 };
 
-const getDates = (events) => {
-  return new Promise((resolve) => {
+const getDates = events => {
+  return new Promise(resolve => {
     resolve({
-      dates:_.uniq(events.reduce((dates, datum) => {
-        dates.push(moment(datum.created_at).format('YYYY-MM-DD'));
-        return dates;
-      }, [])),
+      dates: _.uniq(
+        events.reduce((dates, datum) => {
+          dates.push(moment(datum.created_at).format('YYYY-MM-DD'));
+          return dates;
+        }, [])
+      ),
       events: events
     });
   });
 };
 
-const getDaysInRow = (data) => {
-  return new Promise((resolve) => {
+const getDaysInRow = data => {
+  return new Promise(resolve => {
     const today = process.env.TodaysDate || moment().format('YYYY-MM-DD');
     let daysInRow = 0;
     for (let i = 0; i < data.events.length; i++) {
-      const date = moment(today, 'YYYY-MM-DD').subtract(i + 1, 'day').format('YYYY-MM-DD');
+      const date = moment(today, 'YYYY-MM-DD')
+        .subtract(i + 1, 'day')
+        .format('YYYY-MM-DD');
       if (data.dates.indexOf(date) > -1) daysInRow++;
       else break;
     }
     if (daysInRow >= process.env.Streak) {
-      resolve(`Your GitHub contribution streak is ${daysInRow} days 🙀\n\n*Time to take a break!*`);
+      resolve(
+        `Your GitHub contribution streak is ${daysInRow} days 🙀\n\n*Time to take a break!*`
+      );
     } else resolve();
   });
 };
 
-const getEvents = (data) => {
-  return new Promise((resolve) => {
-    let events = process.env.EventsJSON ? JSON.parse(process.env.EventsJSON) : data;
+const getEvents = data => {
+  return new Promise(resolve => {
+    let events = process.env.EventsJSON
+      ? JSON.parse(process.env.EventsJSON)
+      : data;
     // Optional: exclude certain events.
     // See: https://developer.github.com/v3/activity/events/types/
     // Example: ['IssueCommentEvent', 'GollumEvent'];
     const exclude = process.env.ExcludeEvents || [];
-    if (exclude.length > 0) events = _.filter(events, (item) => exclude.indexOf(item.type) == -1);
+    if (exclude.length > 0)
+      events = _.filter(events, item => exclude.indexOf(item.type) == -1);
     resolve(events);
   });
 };
